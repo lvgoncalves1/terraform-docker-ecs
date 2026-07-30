8fcefd55edb2b5a8295aa7ea418cb7f58ecaa3d6
Lucas <lucasverissimo1799@gmail.com>
2026-07-30 10:12:25 -0300

    configura pra EKS

--- infra/grupoSeguranca.tf
+++ infra/grupoSeguranca.tf
@@ -1,25 +1,25 @@
-resource "aws_security_group" "alb" {
-  name        = "alb_ecs"
+resource "aws_security_group" "ssh-cluster" {
+  name        = "ssh-cluster"
   vpc_id      = module.vpc.vpc_id
 }
 
 
-resource "aws_security_group_rule" "tcp_alb_in" {
+resource "aws_security_group_rule" "ssh-cluster-in" {
   type              = "ingress"
-  from_port         = 8000
-  to_port           = 8000
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.ssh-cluster.id
 }
 
-resource "aws_security_group_rule" "tcp_alb_out" {
+resource "aws_security_group_rule" "ssh-cluster-out" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.ssh-cluster.id
 }
 
 resource "aws_security_group" "privado" {
